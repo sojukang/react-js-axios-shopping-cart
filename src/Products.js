eb8d05df7fb1c102960d3ebb3c369a752b182175
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-06-18 20:56:28 +0900

    feat: Products 함수 useState -> useReducer 로 변경

--- src/Products.js
+++ src/Products.js
@@ -1,29 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import axios from "axios";
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                data: null,
+                error: null
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                data: action.data,
+                error: null
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                data: null,
+                error: action.error
+            };
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
 function Products() {
-    const [products, setProducts] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+    const [state, dispatch] = useReducer(reducer, {
+        loading: false,
+        data: null,
+        error: null
+    });
 
     const fetchProducts = async () => {
+        dispatch({type: 'LOADING'});
         try {
-            setError(null);
-            setProducts(null);
-            setLoading(true);
             const response = await axios.get(
                 'http://13.209.50.192:8080/api/products/'
             );
-            setProducts(response.data);
+            dispatch({type: 'SUCCESS', data: response.data});
         } catch (e) {
-            setError(e);
+            dispatch({type: 'ERROR', error: e});
         }
-        setLoading(false);
     };
 
     useEffect(() => {
         fetchProducts();
     }, []);
+
+    const {loading, data: products, error} = state;
 
     if (loading) return <div>로딩 중..</div>;
     if (error) return <div>에러가 발생했습니다.</div>;
@@ -36,7 +62,7 @@ function Products() {
                     <li key={product.productId}>
                         {product.name}
                     </li>
-                    ))}
+                ))}
             </ul>
             <button onClick={fetchProducts}>다시 불러오기</button>
         </>
